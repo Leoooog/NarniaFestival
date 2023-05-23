96479d48d6d2013fed24bca9f68bad43ab65f4e1
Leoooog <44928044+Leoooog@users.noreply.github.com>
2023-05-23 09:55:23 +0200

    Update routes.php, added /utenti/me endpoint

--- api/app/routes.php
+++ api/app/routes.php
@@ -10,6 +10,12 @@ $app->get('/utenti', function ($request, $response, $args) {
     return $controller->index($request, $response, $args);
 })->add(new JwtMiddleware(['admin']));
 
+$app->get('/utenti/me', function ($request, $response, $args) {
+    global $container;
+    $controller = $container->get('UserController');
+    return $controller->me($request, $response, $args);
+})->add(new JwtMiddleware(['iscritto', 'ospite', 'ristorante', 'admin']));
+
 $app->get('/utenti/{id}', function ($request, $response, $args) {
     global $container;
     $controller = $container->get('UserController');
